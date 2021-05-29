d56c7fdb2895430905efc88ce0327095477f55de
scripka <olga.morgan17@gmail.com>
2021-05-28 22:03:11 -0600

    Clean original react component

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Sidebar from '../Sidebar/Sidebar'
+
+function App() {
+  return (
+    <main className="App">
+     <h2>Let's Build a Discord Clone!!!</h2>
+     <Sidebar />
+    </main>
+  );
+}
+
+export default App;
